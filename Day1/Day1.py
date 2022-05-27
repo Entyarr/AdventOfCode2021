31d91d02ad8910f964134111fa1cd87e28132ff2
Entyarr <domkve@gmail.com>
2022-05-27 11:38:56 +0300

    Create AdventOfCodee2021
    Edited readme.md, push Day1 .py file

---
+++ Day1/Day1.py
@@ -0,0 +1,23 @@
+f = open("input.txt", 'r')
+prevDepth = int(f.readline())
+count = 0
+print(prevDepth,"Nothing to measure")
+for x in f:
+    x = x.strip()
+    if int(x) > prevDepth:
+        print(x, "Increased")
+        count += 1
+    elif int(x) == prevDepth:
+        print(x, "Didnt change")
+    else:
+        print(x, "Decreased")
+    prevDepth = int(x)
+print("Depth increased", count, " times")
+
+#LEARNED
+#As a person coming from C#, this is what I learned:
+#How to read from files
+#How to create variables (apparently they are assigned automatically what the heck?
+#How to print multiple variables
+#How for loops work (seriously what the hell is this
+#count++ doesn't work :pepehands:
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AdventOfCode2021
  Trying to learn Python with Advent of Code 2021
+
+ In each .py file I will log what I learned
